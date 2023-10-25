54c25ee856e4ac9ed13be8e1c2d8b5155ce295b3
Dreamilyy <dirmaa6110110348@gmail.com>
2023-10-25 15:28:46 +0700

    First Deploy dev portfolio Project

---
+++ src/@Component/nameFirstPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function nameFirstPage() {
+  return (
+    <div>Hi My name is Pumin Singhapon</div>
+  )
+}
+
+export default nameFirstPage
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import NameFirstPage from "./@Component/nameFirstPage";
+
+function App() {
+  return (
+    <React.Fragment>
+      <NameFirstPage />
+    </React.Fragment>
+  );
+}
+
+export default App;
