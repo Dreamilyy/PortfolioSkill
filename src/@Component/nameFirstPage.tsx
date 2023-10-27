6b21095760b4ba680c148d52841f1f24237a8c71
LunG DreaM <dirmaa6110110348@gmail.com>
2023-10-27 22:04:36 +0700

    tailwind install

--- src/@Component/nameFirstPage.tsx
+++ src/@Component/nameFirstPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function nameFirstPage() {
   return (
-    <div>Hi My name is Pumin Singhapon</div>
+    <div className="text-3xl font-bold underline">Hi My name is Pumin Singhapon</div>
   )
 }
 
